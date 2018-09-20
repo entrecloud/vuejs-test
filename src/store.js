f539919d2a4607a185cb1a91365f3460a9113c2f
Pianist038801 <maximivanov818@gmail.com>
2018-09-20 04:21:47 +0400

    test project for upwork job

--- src/store.js
+++ src/store.js
@@ -11,6 +11,41 @@ export default new Vuex.Store({
       complete: false
     }]
   },
-  mutations: {},
-  actions: {}
+  mutations: {
+    addTodo (state, newString) {
+      const newItem = {
+        id: parseInt(Math.random() * 1000),
+        title: newString,
+        complete: false
+      }
+      state.todos.push(newItem)
+    },
+    removeTodo (state, item) {
+      const index = state.todos.findIndex(element => element.id === item.id)
+      state.todos.splice(index, 1)
+    },
+    updateTodo (state, item) {
+      const index = state.todos.findIndex(element => element.id === item.id)
+      const tempTodos = [...state.todos]
+      tempTodos[index] = item
+      state.todos = tempTodos
+    }
+  },
+  actions: {
+    addNewTodo ({commit, state}, newString) {
+      commit('addTodo', newString)
+    },
+    updateTodo ({commit, state}, item) {
+      item.complete = !item.complete
+      commit('updateTodo', item)
+    },
+    closeItem ({commit, state}, item) {
+      if (item.status) {
+        commit('removeTodo', item)
+      } else {
+        item.status = 'archived'
+        commit('updateTodo', item)
+      }
+    }
+  }
 })
